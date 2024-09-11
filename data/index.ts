dbe3acb3a2de8a7dc4a2e3124311b64701744cd3
GreatnessGrace <graceunstoppable7@gmail.com>
2024-09-11 16:15:44 +0530

    data: index

---
+++ data/index.ts
@@ -0,0 +1,218 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    // { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with tech stack",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a optimized Pipeline for CI/CD",
+      description: "Diving into Devops",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.png",
+    //   img: "/b5_c.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Task Manager APP",
+      des: "Created a dynamic task management application using Next.js and React for the frontend, and Node.js with Express for the backend.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/GreatnessGrace/Task-Manager",
+    },
+    {
+      id: 2,
+      title: "Video Streaming Application",
+      des: "Developed the backend for a video streaming application with features such as user signup, login, social login, trending videos, random videos, subscriptions, and video search.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/GreatnessGrace/VideoStreamingApplication",
+    },
+    {
+      id: 3,
+      title: "Scalable Chat Application",
+      des: "Scalable Chat Application with Redis.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/GreatnessGrace/Scalable-Chat-Application",
+    },
+    {
+      id: 4,
+      title: "DOM Projects",
+      des: "Some practical working on DOM.",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/GreatnessGrace/DOM-Projects",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Ankush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ankush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ankush is the ideal partner.",
+      name: "Grace",
+      title: "Director of ABCD Technologies",
+    },
+    {
+        quote:
+        "Collaborating with Ankush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ankush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ankush is the ideal partner.",
+      name: "Grace",
+      title: "Director of ABCD Technologies",
+    },
+    {
+        quote:
+        "Collaborating with Ankush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ankush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ankush is the ideal partner.",
+      name: "Grace",
+      title: "Director of ABCD Technologies",
+    },
+    {
+        quote:
+        "Collaborating with Ankush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ankush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ankush is the ideal partner.",
+      name: "Grace",
+      title: "Director of ABCD Technologies",
+    },
+    {
+        quote:
+        "Collaborating with Ankush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ankush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ankush is the ideal partner.",
+      name: "Grace",
+      title: "Director of ABCD Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Relinns Technologies",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "iLeads",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "e-Solutions",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "Cdac",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Software Engineer Intern",
+      desc: "Assisted in the development of a ChatBot and other various client projects  using Node.js, enhancing security.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Cdac-iLeads",
+      desc: "Developed and Deployed various web-applications.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Cdac-e-Solutions",
+      desc: "Shifted Deployment process to docker via gitlab automation.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Freelance Developer",
+      desc: "Developed and maintained user-facing features using modern technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
