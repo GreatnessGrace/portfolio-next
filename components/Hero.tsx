8e10f5817d361d02968a3b7b06d22302aded0126
GreatnessGrace <graceunstoppable7@gmail.com>
2024-09-12 09:06:44 +0530

    Renaming Text Generator

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
-import { TextGenerateEffect } from './ui/text-generate-effect'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import MagicButton from './MagicButton'
 import { FaLocationArrow } from "react-icons/fa6";
 
